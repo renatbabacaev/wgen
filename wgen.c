ad2c41b6d149238514bed48da958fc7a270b8059
Renat Babacaev <renat.babacaev@gmail.com>
2021-05-06 19:05:15 +0300

    📝: Edit functions in wgen.c and filenames for headers

--- wgen.c
+++ wgen.c
@@ -5,34 +5,11 @@
 #include "own/colchar.h"        // Collecting characters from a string
 #include "own/len.h"            // Getting String Lenght
 #include "own/remchar.h"        // Removing same characters
+#include "own/output.h"
 
 
 char word[255], prefix[255];
 int word_count, max_length;
-
-int output(int level, int plevel)
-{
-    int i;
-
-    if (level == plevel)
-    {
-        for(i = 0; i < word_count; i++)
-        {
-            printf("%s%c\n", prefix, word[i]);
-        }
-    }
-
-    else
-    {
-        for(i = 0; i < word_count; i++)
-        {
-            prefix[level] = word[i];
-            output(level + 1, plevel); 
-        }
-    }
-
-    return 0;
-}
 
 int main()
 {
@@ -44,17 +21,17 @@ int main()
     printf("Maximum length: ");
     scanf("%i", &max_length);
 
-    word_count = len(word);
-    colchar(word_count, word);
-    remchar(word_count, word);
-    word_count = len(word);
+    word_count = LEN(word);
+    COLCHAR(word_count, word);
+    REMCHAR(word_count, word);
+    word_count = LEN(word);
 
     printf("Minimum lenght: ");
     scanf("%i", &i); i--;
 
     for(; i < max_length; i++)
     {
-        output(0, i);
+        OUTPUT(0, i);
     }
 
     return 0;
